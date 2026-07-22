b0cd9b06315483b3484172e6c377f30f0bc2f48c
mealworm2004hiki-a11y <mealworm2004hiki@gmail.com>
2026-07-22 17:33:00 +0900

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE='kakei-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-maskable.svg'];
+self.addEventListener('install',e=>{
+self.skipWaiting();
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+});
+self.addEventListener('activate',e=>{
+e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+if(e.request.method!=='GET')return;
+e.respondWith(
+caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
+const copy=res.clone();
+caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+return res;
+}).catch(()=>caches.match('./index.html')))
+);
+});
